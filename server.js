5537a702d04239410c1ce0c740533e6dec429246
alexismenendez <amenendez146@gmail.com>
2023-12-07 13:33:13 -0600

    added middleware, routes, and port to server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = 3001;
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+
+//Homepage Route
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//Note Page Route
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//Deployment
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
